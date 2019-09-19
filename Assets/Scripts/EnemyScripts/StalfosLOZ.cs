c75dc79c86b489d9934a5219e79d7a67af97c12a
kidagine <davidkalatzis1999@hotmail.com>
2019-09-19 21:11:29 +0200

    +Started work on basic player attacking. +Started work on basic enemy interactions.

---
+++ Assets/Scripts/EnemyScripts/StalfosLOZ.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class StalfosLOZ : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _stalfosRigidbody = default;
+    private int _health = 2;
+
+    Vector2 test = new Vector2(-2, 0);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerAttack"))
+        {
+            Vector2 difference = transform.position - other.transform.position;
+            _stalfosRigidbody.velocity = difference;
+            Damaged();
+        }
+    }
+
+    private void Damaged()
+    {
+        _health--;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerScripts/LinkLOZMovement.cs
+++ Assets/Scripts/PlayerScripts/LinkLOZMovement.cs
@@ -1,17 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class LinkLOZMovement : MonoBehaviour
 {
-    [SerializeField] private Animator _playerAnimator = default;
-    [SerializeField] private Rigidbody2D _playerRigidbody = default;
+    [SerializeField] private Animator _linkAnimator = default;
+    [SerializeField] private Rigidbody2D _linkRigidbody = default;
     private Vector2 _direction;
     private Vector2 _lastDirection;
+    private bool _isAttacking;
     private readonly int _moveSpeed = 3;
 
 
     void Update()
     {
         CheckPlayerDirection();
+        Attack();
     }
 
     private void FixedUpdate()
@@ -21,38 +24,65 @@ public class LinkLOZMovement : MonoBehaviour
 
     private void CheckPlayerDirection()
     {
-        if (_direction.y != 1 && _direction.y != -1)
+        if (!_isAttacking)
         {
-            _direction.x = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
-            if (_direction.x != 0)
+            if (_direction.y != 1 && _direction.y != -1)
             {
-                _playerAnimator.SetFloat("Vertical", 0);
+                _direction.x = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+                if (_direction.x != 0)
+                {
+                    _linkAnimator.SetFloat("Vertical", 0);
 
-                _lastDirection.x = _direction.x;
-                _playerAnimator.SetFloat("Horizontal", _lastDirection.x);
-                _playerAnimator.speed = 1;
+                    _lastDirection.x = _direction.x;
+                    _linkAnimator.SetFloat("Horizontal", _lastDirection.x);
+                    _linkAnimator.speed = 1;
+                }
+            }
+            if (_direction.x != 1 && _direction.x != -1)
+            {
+                _direction.y = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+                if (_direction.y != 0)
+                {
+                    _linkAnimator.SetFloat("Horizontal", 0);
+
+                    _lastDirection.y = _direction.y;
+                    _linkAnimator.SetFloat("Vertical", _lastDirection.y);
+                    _linkAnimator.speed = 1;
+                }
+             }
+            if (_direction == Vector2.zero)
+            {
+                _linkAnimator.speed = 0;
             }
         }
-        if (_direction.x != 1 && _direction.x != -1)
+        else
         {
-            _direction.y = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
-            if (_direction.y != 0)
-            {
-                _playerAnimator.SetFloat("Horizontal", 0);
-
-                _lastDirection.y = _direction.y;
-                _playerAnimator.SetFloat("Vertical", _lastDirection.y);
-                _playerAnimator.speed = 1;
-            }
-        }
-        if (_direction == Vector2.zero)
-        {
-            _playerAnimator.speed = 0;
+            _linkAnimator.speed = 1;
         }
     }
 
     private void Movement()
     {
-        _playerRigidbody.MovePosition(_playerRigidbody.position + _direction * _moveSpeed * Time.fixedDeltaTime);
+        if (!_isAttacking)
+        {
+            _linkRigidbody.MovePosition(_linkRigidbody.position + _direction * _moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isAttacking = true;
+            _linkAnimator.SetBool("IsAttacking", _isAttacking);
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(0.4f);
+        _isAttacking = false;
+        _linkAnimator.SetBool("IsAttacking", _isAttacking);
     }
 }
